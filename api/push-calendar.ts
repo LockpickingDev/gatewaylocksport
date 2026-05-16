5e2aa2b83b8cf0126264faba9b7e0554ac233368
LockpickingDev <lockpicking.dev@gmail.com>
2026-05-16 11:17:07 -0500

    Fixing corrupted newlines

--- api/push-calendar.ts
+++ api/push-calendar.ts
@@ -15,10 +15,17 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   }
 
   try {
+    const privateKey = process.env.GOOGLE_PRIVATE_KEY
+      ? process.env.GOOGLE_PRIVATE_KEY
+          .replace(/\\n/g, '\n')
+          .replace(/\\r/g, '')
+          .replace(/"/g, '')
+      : ''
+
     const auth = new GoogleAuth({
       credentials: {
         client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-        private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n')
+        private_key: privateKey
       },
       scopes: ['https://www.googleapis.com/auth/calendar.events']
     })
